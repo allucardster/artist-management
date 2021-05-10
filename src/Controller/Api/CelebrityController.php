a11da31cdd29811632a6c33c467e167d0d354036
Richard Melo <allucardster@gmail.com>
2021-05-10 02:34:19 -0500

    Add `Celebrity` controller

---
+++ src/Controller/Api/CelebrityController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Repository\CelebrityRepository;
+use FOS\RestBundle\Controller\Annotations as Rest;
+
+/**
+ * Class CelebrityController
+ *
+ * @Rest\Route("/celebrity")
+ *
+ * @package App\Controller\Api
+ */
+class CelebrityController
+{
+}
