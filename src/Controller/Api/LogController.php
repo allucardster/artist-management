657bce336b850348f5db12b07af6d8b4caea7093
Richard Melo <allucardster@gmail.com>
2021-05-11 19:06:25 -0500

    Allow list log entry for any authenticated user

--- src/Controller/Api/LogController.php
+++ src/Controller/Api/LogController.php
@@ -6,13 +6,11 @@ use App\Pagination\PaginationQuery;
 use App\Pagination\PaginationResult;
 use App\Repository\LogRepository;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration as Sensio;
 
 /**
  * Class LogController
  *
  * @Rest\Route("/log")
- * @Sensio\IsGranted("ROLE_ADMIN")
  *
  * @package App\Controller\Api
  */
